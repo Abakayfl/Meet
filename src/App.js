5a82a1e0eeda25bf2d574a0c1217b726736252ad
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2021-03-10 16:22:48 -0500

    Fixing Expected and Received Values

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import './App.css';
+import EventList from './EventList';
+import CitySearch from './CitySearch';
+import NumberOfEvents from './NumberOfEvents';
+import { getEvents, extractLocations } from './api';
+import "./nprogress.css";
+// import { ErrorAlert } from './Alert';
+
+class App extends Component {
+
+  state = {
+    events: [],
+    locations: [],
+    numberOfEvents: '24',
+    currentLocation: "all"
+  }
+
+  updateEvents = (location, eventCount) => {
+    const { currentLocation, numberOfEvents } = this.state;
+    if (location) {
+      getEvents().then((events) => {
+        const locationEvents =
+          location === "all"
+            ? events
+            : events.filter((event) => event.location === location);
+        const filteredEvents = locationEvents.slice(0, numberOfEvents);
+        this.setState({
+          events: filteredEvents,
+          currentLocation: location,
+        });
+      });
+    } else {
+      getEvents().then((events) => {
+        const locationEvents =
+          currentLocation === "all"
+            ? events
+            : events.filter((event) => event.location === currentLocation);
+        const filteredEvents = locationEvents.slice(0, eventCount);
+        this.setState({
+          events: filteredEvents,
+          numberOfEvents: eventCount,
+        });
+      });
+    }
+  }
+
+  ifOnline = () => {
+    if (!navigator.onLine) {
+      this.setState({
+        infoText: 'You are currently offline- events may not be up to date'
+      })
+    } else {
+      return this.setState({
+        infoText: '',
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+    this.ifOnline();
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="header-container">
+          <h1 id="header"><span id="devel">devel_</span><span id="up">Up</span><span id="plus">+</span></h1>
+          {/* <p className="error-alert-offline" ><ErrorAlert text={this.state.infoText} /></p> */}
+        </div>
+        <br></br><br></br>
+          <p id="subheader">Want to level-up your dev skills?<br/><br/>Explore upcoming events in <span id="webdev-header">Web Development</span> from around the world!</p>
+        <br/><br/>
+        <label id="city-label">Select your city: 
+          <br/><br/><br/>
+          <CitySearch 
+            locations={this.state.locations} 
+            updateEvents={this.updateEvents} 
+          />
+        </label>
+        <br/><br/>
+        <NumberOfEvents 
+          numberOfEvents={this.state.numberOfEvents}
+          updateEvents={this.updateEvents}
+        />
+        <br></br>
+        <EventList events={this.state.events}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/NumberOfEvents.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+// import { ErrorAlert } from './Alert';
+
+class NumberOfEvents extends Component {
+
+  state = {
+    numberOfEvents: '24',
+  };
+
+  // When user changes number value
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.props.updateEvents(null, value);
+    if (value < 0 | value >= 101) {
+      this.setState({
+        infoText: 'Please enter a number between 0 and 100'
+      })
+    } else {
+      return this.setState({
+        infoText: '',
+        numberOfEvents: value
+      })
+    }
+  };
+
+  render() {
+    
+    const { numberOfEvents } = this.state;
+    
+    return (
+      <div className="numberOfEvents">
+        {/* <p className="error-alert" ><ErrorAlert text={this.state.infoText} /></p> */}
+        <label id="numevents-label">Number of events: 
+          <br/><br/><br></br>
+          <input
+            type='number'
+            className='number'
+            value={numberOfEvents}
+            onChange={this.handleChange}
+          />
+        </label>
+      </div>
+    );
+  }
+}
+
+export default NumberOfEvents;
