28ca15573b50cd460cf896332456894d8e1401ba
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2021-03-15 23:04:18 -0400

    offline feature

--- src/api.js
+++ src/api.js
@@ -63,7 +63,7 @@ export const getEvents = async () => {
         NProgress.done();
         return {
             events: JSON.parse(events).events,
-            locations: extractLocations(JSON.parse(events).events)
+            locations: extractLocations(JSON.parse(events).events),
         };
     }
 
